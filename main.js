08396788f222a3b5b66977ef2bb2f3f31dfc230d
Dis691 <113673860+Dis691@users.noreply.github.com>
2023-09-12 20:59:00 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Evgen the best teacher!'; /* The text */
+var txt = 'click me!'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
